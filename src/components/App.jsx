14d43e09c5ffb5513d0af562fa504f1f2f71c1b7
David <dmchoull@gmail.com>
2018-04-22 15:02:00 -0400

    moved provider/store into App component so it works in production build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Router } from 'react-static';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import Routes from 'react-static-routes';
+import reducer from '../reducers';
 import '../assets/stylesheets/app.pcss';
 
+const store = createStore(reducer);
+
 const App = () => (
-  <Router>
-    <Routes />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Routes />
+    </Router>
+  </Provider>
 );
 
 export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
 import App from './components/App';
 
 // Export your top level component as JSX (for static rendering)
@@ -12,13 +9,10 @@ export default App;
 
 // Render your app
 if (typeof document !== 'undefined') {
-  const store = createStore(reducer);
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render;
   const render = (Comp) => {
     renderMethod(
-      <Provider store={store}>
-        <Comp />
-      </Provider>,
+      <Comp />,
       document.getElementById('root'),
     );
   };
