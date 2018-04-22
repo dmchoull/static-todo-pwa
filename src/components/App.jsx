14dde066ecca202ecf6d7787b474e324de7646b0
David <dmchoull@gmail.com>
2018-04-22 18:56:27 -0400

    added redux enhancer + middleware for persisting state to localStorage and logging actions in development

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Router } from 'react-static';
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import persistState, { mergePersistedState } from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
 import { Provider } from 'react-redux';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import Routes from 'react-static-routes';
 import reducer from '../reducers';
 import '../assets/stylesheets/app.pcss';
 
-const store = createStore(reducer);
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+let enhancer = compose(applyMiddleware(...middlewares));
+
+if (typeof document !== 'undefined') {
+  // use persistState enhancer if we're running in the browser
+  const storage = adapter(window.localStorage);
+  enhancer = compose(
+    applyMiddleware(...middlewares),
+    persistState(storage, 'todo-app'),
+  );
+}
+
+const store = createStore(
+  mergePersistedState()(reducer),
+  {},
+  enhancer,
+);
 
 const App = () => (
   <Provider store={store}>
